321499d63eff58b5653ae7d13483bc6481e14bdc
Gautam Deshpande <gdeshpande14@gmail,com>
2020-12-07 21:46:26 +0530

    home page

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -5,11 +5,71 @@ import 'package:flutter/material.dart';
 class Home extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Center(child: Text('Home Page')),
-        backgroundColor: Colors.blueGrey[900],
-      ),
+    return MaterialApp(
+      home : Scaffold(
+        appBar: AppBar(
+
+          backgroundColor: Colors.blueGrey[800],
+            title : SizedBox(
+              child : Row(
+                children: [
+                  Center(child: Text('Home Page')),
+                  new DropdownButton<String>(
+                      icon: Icon(Icons.account_circle),
+                      iconSize: 24,
+                      elevation: 16,
+                      items: <String>['Orders','Profile','Log Out'].map((String value) {
+                        return new DropdownMenuItem<String>(
+                          value: value,
+                          child: new Text(value),
+                        );
+                      }).toList(),
+                      onChanged: (_) {},
+                    ),
+                  ],
+                  ),
+              ),
+            ),
+        ),
     );
   }
 }
+
+/*class dropdown extends StatefulWidget {
+  dropdown({Key key}) : super(key: key);
+
+  @override
+  _dropdown createState() => _dropdown();
+}
+
+
+class _dropdown extends State<dropdown> {
+  String dropdownValue;
+
+  @override
+  Widget build(BuildContext context) {
+    return DropdownButton<String>(
+      value: dropdownValue,
+      icon: Icon(Icons.account_circle),
+      iconSize: 24,
+      elevation: 16,
+      style: TextStyle(color: Colors.white),
+      underline: Container(
+        height: 2,
+        color: Colors.black,
+      ),
+      onChanged: (String newValue) {
+        setState(() {
+          dropdownValue = newValue;
+        });
+      },
+      items: <String>['Orders', 'Profile', 'Log Out']
+          .map<DropdownMenuItem<String>>((String value) {
+        return DropdownMenuItem<String>(
+          value: value,
+          child: Text(value),
+        );
+      }).toList(),
+    );
+  }
+}*/
