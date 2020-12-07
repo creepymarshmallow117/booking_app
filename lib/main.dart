d6b0ce2c036c437ae5fef56c5c98dc205e706e3c
Gautam Deshpande <gdeshpande14@gmail,com>
2020-12-07 13:00:57 +0530

    home page

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:booking_app/screens/home.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -8,8 +9,6 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-
-    );
+    return Home();
   }
 }
