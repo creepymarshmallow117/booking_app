4dc444ef11f207d3365b1b274140c9c1e42aa811
Gautam Deshpande <gdeshpande14@gmail,com>
2021-01-12 14:59:24 +0530

    Added search bar and updated text in login and registration page. Also, changed classes from stateless to statefull in orders and profile file.

--- lib/screens/authenticate/register.dart
+++ lib/screens/authenticate/register.dart
@@ -1,6 +1,6 @@
 import 'dart:io';
 
-import 'file:///C:/Users/Aditya/AndroidStudioProjects/booking_app/lib/screens/home/home.dart';
+import 'file:///D:/College/Project/App/lib/screens/home/home.dart';
 import 'package:path/path.dart';
 import 'package:booking_app/services/auth.dart';
 import 'package:firebase_storage/firebase_storage.dart';
@@ -45,99 +45,102 @@ class _RegisterState extends State<Register> {
         padding: EdgeInsets.symmetric(vertical: 20.0, horizontal: 50.0),
         child: Form(
           key: formKey,
-          child: Column(
-              children: <Widget>[
-              SizedBox(height: 20.0),
-            TextFormField(
-                validator: (val) => val.isEmpty ? "Enter email" : null,
-                onChanged: (val){
-                  print(val);
-                  setState(() => email = val);
-                }
-                ,decoration: new InputDecoration(
-                hintText: 'Enter Email'
-            )
-            ),
-                SizedBox(height: 20.0),
-                TextFormField(
-                  obscureText: true,
-                  validator: (val) => val.length < 6 ? "Enter a password more than 6 characters long" : null,
-                  onChanged: (val){
-                    setState(() => password = val);
-                  },
-                  decoration: new InputDecoration(
-                      hintText: 'Enter Password'
-                  ),
-                ),
-                SizedBox(height: 20.0),
-                TextFormField(
-                  validator: (val) => password != val ? "Password aren't the same": null,
-                  obscureText: true,
-                  decoration: new InputDecoration(
-                      hintText: 'Re-enter Password'
-                  ),
-                ),
-                SizedBox(height: 20.0),
-                TextFormField(
-                  validator: (val) => displayName.isEmpty ? "a display name is required": null,
-                  decoration: new InputDecoration(
-                      hintText: 'Enter Display Name'
-                  ),
-                  onChanged: (val){
-                    setState(() {
-                      displayName = val;
-                    });
-                  },
-                ),
-                SizedBox(height: 20.0),
-                RaisedButton(
-                  color: Colors.red[200],
-                  child: Text('Upload Image', style: TextStyle(color: Colors.brown[400])),
-                  onPressed: () async{
-                    File _image;
-                    final picker = ImagePicker();
-                    final pickedFile = await picker.getImage(source: ImageSource.gallery);
-                    setState(() {
-                      _image = File(pickedFile.path);
-                    });
-                    String fileName = basename(_image.path);
-                    Reference firebaseStorageRef =
-                    FirebaseStorage.instance.ref().child('uploads/$fileName');
-                    UploadTask uploadTask = firebaseStorageRef.putFile(_image);
-                    TaskSnapshot taskSnapshot = await uploadTask;
-                    taskSnapshot.ref.getDownloadURL().then(
-                          (value) => print("Done: $value"),
-                    );
-                  },
-                ),
-                SizedBox(height: 20.0),
-                RaisedButton(
-                    color: Colors.red[200],
-                    child: Text('Register', style: TextStyle(color: Colors.brown[400])),
-                    onPressed: () async{
-                      print(email);
-                      print(password);
-                      if (formKey.currentState.validate()){
-                        dynamic result = await auth.registerWithEmail(email, password, displayName);
-                        if(result == null){
-                          setState((){
-                            error = 'please supply a valid email';
-                          });
+          child: SingleChildScrollView(
+              child: Column(
+                  children: <Widget>[
+                    SizedBox(height: 20.0),
+                    TextFormField(
+                        validator: (val) => val.isEmpty ? "Enter email" : null,
+                        onChanged: (val){
+                          print(val);
+                          setState(() => email = val);
                         }
-                        else{
-                          Navigator.push(
-                            context,
-                            MaterialPageRoute(builder: (context) => Home()),
-                          );
-                        }
-                      }
-                    }),
-                Text(
-                  error,
-                  style: TextStyle(color: Colors.red, fontSize: 14.0),
-                )
-            ]
-        )
+                        ,decoration: new InputDecoration(
+                        hintText: 'Enter Email'
+                    )
+                    ),
+                    SizedBox(height: 20.0),
+                    TextFormField(
+                      obscureText: true,
+                      validator: (val) => val.length < 6 ? "Enter a password more than 6 characters long" : null,
+                      onChanged: (val){
+                        setState(() => password = val);
+                      },
+                      decoration: new InputDecoration(
+                          hintText: 'Enter Password'
+                      ),
+                    ),
+                    SizedBox(height: 20.0),
+                    TextFormField(
+                      validator: (val) => password != val ? "Passwords do not match": null,
+                      obscureText: true,
+                      decoration: new InputDecoration(
+                          hintText: 'Re-enter Password'
+                      ),
+                    ),
+                    SizedBox(height: 20.0),
+                    TextFormField(
+                      validator: (val) => displayName.isEmpty ? "A display name is required": null,
+                      decoration: new InputDecoration(
+                          hintText: 'Enter Display Name'
+                      ),
+                      onChanged: (val){
+                        setState(() {
+                          displayName = val;
+                        });
+                      },
+                    ),
+                    SizedBox(height: 20.0),
+                    RaisedButton(
+                      color: Colors.red[200],
+                      child: Text('Upload Image', style: TextStyle(color: Colors.brown[400])),
+                      onPressed: () async{
+                        File _image;
+                        final picker = ImagePicker();
+                        final pickedFile = await picker.getImage(source: ImageSource.gallery);
+                        setState(() {
+                          _image = File(pickedFile.path);
+                        });
+                        String fileName = basename(_image.path);
+                        Reference firebaseStorageRef =
+                        FirebaseStorage.instance.ref().child('uploads/$fileName');
+                        UploadTask uploadTask = firebaseStorageRef.putFile(_image);
+                        TaskSnapshot taskSnapshot = await uploadTask;
+                        taskSnapshot.ref.getDownloadURL().then(
+                              (value) => print("Done: $value"),
+                        );
+                      },
+                    ),
+                    SizedBox(height: 20.0),
+                    RaisedButton(
+                        color: Colors.red[200],
+                        child: Text('Register', style: TextStyle(color: Colors.brown[400])),
+                        onPressed: () async{
+                          print(email);
+                          print(password);
+                          if (formKey.currentState.validate()){
+                            dynamic result = await auth.registerWithEmail(email, password, displayName);
+                            if(result == null){
+                              setState((){
+                                error = 'Please supply a valid email';
+                              });
+                            }
+                            else{
+                              Navigator.push(
+                                context,
+                                MaterialPageRoute(builder: (context) => Home()),
+                              );
+                            }
+                          }
+                        }),
+                    Text(
+                      error,
+                      style: TextStyle(color: Colors.red, fontSize: 14.0),
+                    )
+                  ]
+              )
+          )
+
       ),
       )
     );
